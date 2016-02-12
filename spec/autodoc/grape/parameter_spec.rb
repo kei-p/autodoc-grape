da79bb67789f9917c580cb36ffb7c276a31e792b
Keisuke Ishizawa <k_ishizawa@sfidante.co.jp>
2016-02-12 17:30:15 +0900

    Refactor spec grouping by case

--- spec/autodoc/grape/parameter_spec.rb
+++ spec/autodoc/grape/parameter_spec.rb
@@ -1,9 +1,9 @@
 class Dummy::API < Grape::API
   params do
     optional :number, type: Integer
-    requires :text, type: String
-    optional :restricted_values, type: Integer, values: [1,2,3]
-    optional :restricted_values_range, type: Integer, values: -10..+10
+    requires :required_value, type: Integer
+    optional :restricted_value, type: Integer, values: [1,2,3]
+    optional :restricted_value_range, type: Integer, values: -10..+10
     optional :desc_value, type: Integer, desc: 'description'
     optional :default_value, type: Integer, default: 0
 
@@ -32,53 +32,57 @@ describe Autodoc::Grape::Document::Parameter do
       it { expect(subject).to eq("* number Integer") }
     end
 
-    context "text" do
-      let(:validator_name) { "text" }
-      it { expect(subject).to eq("* text String (required)") }
-    end
-
-    context "restricted_values" do
-      let(:validator_name) { "restricted_values" }
-      it { expect(subject).to eq("* restricted_values Integer (only: `[1, 2, 3]`)") }
-    end
-
-    context "restricted_values_range" do
-      let(:validator_name) { "restricted_values_range" }
-      it { expect(subject).to eq("* restricted_values_range Integer (only: `-10..10`)") }
-    end
-
-    context "desc_value" do
-      let(:validator_name) { "desc_value" }
-      it { expect(subject).to eq("* desc_value Integer - description") }
-    end
-    context "default_value" do
-      let(:validator_name) { "default_value" }
-      it { expect(subject).to eq("* default_value Integer (default: `0`)") }
-    end
-
     context "array" do
       let(:validator_name) { "array" }
       it { expect(subject).to eq("* array [Integer]") }
     end
 
-    context "hash" do
-      let(:validator_name) { "hash" }
-      it { expect(subject).to eq("* hash Hash") }
+    describe "options" do
+      context "required_value" do
+        let(:validator_name) { "required_value" }
+        it { expect(subject).to eq("* required_value Integer (required)") }
+      end
+
+      context "restricted_value" do
+        let(:validator_name) { "restricted_value" }
+        it { expect(subject).to eq("* restricted_value Integer (only: `[1, 2, 3]`)") }
+      end
+
+      context "restricted_value_range" do
+        let(:validator_name) { "restricted_value_range" }
+        it { expect(subject).to eq("* restricted_value_range Integer (only: `-10..10`)") }
+      end
+
+      context "desc_value" do
+        let(:validator_name) { "desc_value" }
+        it { expect(subject).to eq("* desc_value Integer - description") }
+      end
+      context "default_value" do
+        let(:validator_name) { "default_value" }
+        it { expect(subject).to eq("* default_value Integer (default: `0`)") }
+      end
     end
 
-    context "hash[hash_attr]" do
-      let(:validator_name) { "hash[hash_attr]" }
-      it { expect(subject).to eq(" * hash_attr Integer") }
-    end
+    describe "nested hash" do
+      context "hash" do
+        let(:validator_name) { "hash" }
+        it { expect(subject).to eq("* hash Hash") }
+      end
 
-    context "hash[deep_hash]" do
-      let(:validator_name) { "hash[deep_hash]" }
-      it { expect(subject).to eq(" * deep_hash Hash") }
-    end
+      context "hash[hash_attr]" do
+        let(:validator_name) { "hash[hash_attr]" }
+        it { expect(subject).to eq(" * hash_attr Integer") }
+      end
 
-    context "hash[deep_hash][deep_hash_attr]" do
-      let(:validator_name) { "hash[deep_hash][deep_hash_attr]" }
-      it { expect(subject).to eq("  * deep_hash_attr Integer") }
+      context "hash[deep_hash]" do
+        let(:validator_name) { "hash[deep_hash]" }
+        it { expect(subject).to eq(" * deep_hash Hash") }
+      end
+
+      context "hash[deep_hash][deep_hash_attr]" do
+        let(:validator_name) { "hash[deep_hash][deep_hash_attr]" }
+        it { expect(subject).to eq("  * deep_hash_attr Integer") }
+      end
     end
   end
 end
